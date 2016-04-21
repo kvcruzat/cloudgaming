2141e4ff5c2b3f3988b520b7cbb12167d9b93c24
Khen Cruzat <sc13kvc@leeds.ac.uk>
2016-04-21 23:25:34 +0100

    Added instructions to build odl

--- Implementation/networking/load_balance_RR.bash
+++ Implementation/networking/load_balance_RR.bash
@@ -17,9 +17,11 @@ echo "Creating Pool Members"
 for members in 2 3 4
 do
 
-curl --user "admin":"admin" -H "Accept: application/json" -H "Content-type: application/json" http://127.0.0.1:8080/one/nb/v2/lb/default/create/poolmember -d '{"name":"PM'$members'","ip":"10.0.0.'$members'","poolname":"PoolRR"}'
+curl --user "admin":"admin" -H "Accept: application/json" -H "Content-type: application/json" -X POST http://127.0.0.1:8080/one/nb/v2/lb/default/create/poolmember -d '{"name":"PM'$members'","ip":"10.0.0.'$members'","poolname":"PoolRR"}'
 
 if [[ $? == 0 ]] ; then
 echo " Pool Member PM"$members" Created "
 fi
 done
+
+curl --user "admin":"admin" -H "Accept: application/json" -H "Content-type: application/json" -X GET http://127.0.0.1:8080/one/nb/v2/lb/default/PoolRR/pms
